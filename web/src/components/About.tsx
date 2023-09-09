a8d882019f2b314e69e6a679a9a98408065c22dc
Om Rajpal <omrajpal15@gmail.com>
2023-09-09 16:48:50 -0400

    about us spacing

--- web/src/components/About.tsx
+++ web/src/components/About.tsx
@@ -40,9 +40,10 @@ export const About: React.FC = () => {
                 How It Works
                 </Text>
                 <List
-                spacing="md"
+                spacing="lg"
                 size="sm"
                 center
+                pt='md'
                 // icon={
                 //   <ThemeIcon color="teal" size={24} radius="xl">
                 //     <IconCircleCheck size="1rem" />
@@ -74,9 +75,10 @@ export const About: React.FC = () => {
                 </Text>
 
                 <List
-                spacing="md"
+                spacing="lg"
                 size="md"
                 center
+                pt='md'
                 // icon={
                 //   <ThemeIcon color="teal" size={24} radius="xl">
                 //     <IconCircleCheck size="1rem" />
@@ -85,31 +87,31 @@ export const About: React.FC = () => {
                 >
                     <List.Item>
                         Enhanced Memory Retention
-                        <Text align="left" size="sm" px='lg'>
+                        <Text align="left" size="sm" px='lg' pt='xs'>
                         Mind maps help in better memory retention by utilizing both hemispheres of the brain. They engage your mind visually and analytically, thereby aiding in more effective memorization. According to Dr. Caroline Leaf, a cognitive neuroscientist, visual processes account for up to two-thirds of the electrical activity in the brain's perception, which suggests the importance of visual aids like mind maps in learning and memory (Leaf, Caroline. "Switch On Your Brain." Baker Books, 2013).
                         </Text>
                     </List.Item>
                     <List.Item>
                         Hierarchical Structure for Easy Recollection
-                        <Text align="left" size="sm" px='lg'>
+                        <Text align="left" size="sm" px='lg' pt='xs'>
                         Mind maps have a hierarchical structure that starts with a central idea and branches out into sub-topics and further details. This hierarchization aids in contextual learning and makes it easier for the brain to retrieve specific pieces of information when needed. Research by Nist and Simpson has found that hierarchical structuring is effective for recall tasks (Nist, Sherrie L., and Simpson, Michele L. "College Studying." In: Peverly, Susan et al. "The Skillful Teacher." Allyn & Bacon, 1991).
                         </Text>
                     </List.Item>
                     <List.Item>
                         Active Engagement
-                        <Text align="left" size="sm" px='lg'>
+                        <Text align="left" size="sm" px='lg' pt='xs'>
                         Creating a mind map requires active engagement, requiring you to think critically about the material you are studying. This deep level of processing aids in memory retention. According to the Levels of Processing model by Craik & Lockhart, deeper levels of analysis produce more elaborate, longer-lasting memory codes (Craik, F. I. M., & Lockhart, R. S. "Levels of processing: A framework for memory research." Journal of Verbal Learning and Verbal Behavior, 1972).
                         </Text>
                     </List.Item>
                     <List.Item>
                         Promotes Creative Connections
-                        <Text align="left" size="sm" px='lg'>
+                        <Text align="left" size="sm" px='lg' pt='xs'>
                         Mind maps encourage you to draw connections between disparate pieces of information, which can lead to deeper understanding and better retention. Tony Buzan, an advocate of mind mapping, argues that the associative and integrative capabilities of mind maps facilitate creative thinking and problem-solving (Buzan, Tony. "The Mind Map Book." BBC Books, 1995).
                         </Text>
                     </List.Item>
                     <List.Item>
                         Flexibility
-                        <Text align="left" size="sm" px='lg'>
+                        <Text align="left" size="sm" px='lg' pt='xs'>
                         Mind maps are highly customizable and can be adapted to suit various subjects and learning styles. This flexibility means that they can be a particularly good fit for complex subjects that require the integration of various types of information, as confirmed by a study published in the journal, "Medical Education" (Farrand, Paul, Hussain, Fearzana, and Hennessy, Enid. "The efficacy of the 'mind map' study technique." Medical Education, 2002).
                         </Text>
                     </List.Item>
@@ -119,9 +121,10 @@ export const About: React.FC = () => {
                 Why Choose Us?
                 </Text>
                 <List
-                spacing="xs"
+                spacing="lg"
                 size="sm"
                 center
+                pt='md'
                 listStyleType="disc"
                 // icon={
                 //   <ThemeIcon color="teal" size={24} radius="xl">
