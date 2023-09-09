e72f6b5dd3e73f778f0e3c2fe75f04f19dfd66f1
Eunsoo Shin <me@esinx.net>
2023-09-08 22:28:19 -0400

    🐛 map interaction

--- web/src/components/Map.tsx
+++ web/src/components/Map.tsx
@@ -1,27 +1,31 @@
-import ReactFlow, { Background, BackgroundVariant, Controls } from 'reactflow'
+import ReactFlow, {
+	Background,
+	BackgroundVariant,
+	Controls,
+	useNodesState,
+} from 'reactflow'
 
 import 'reactflow/dist/style.css'
 
 export type MapProps = {}
 
 export const Map: React.FC<MapProps> = () => {
+	const [nodes, setNodes, onNodesChange] = useNodesState([
+		{
+			id: '1',
+			data: { label: 'Node 1' },
+			position: { x: 250, y: 500 },
+			draggable: true,
+		},
+		{
+			id: '2',
+			data: { label: 'Node 2' },
+			position: { x: 500, y: 700 },
+			draggable: true,
+		},
+	])
 	return (
-		<ReactFlow
-			snapToGrid
-			snapGrid={[16, 16]}
-			nodes={[
-				{
-					id: '1',
-					data: { label: 'Node 1' },
-					position: { x: 250, y: 500 },
-				},
-				{
-					id: '2',
-					data: { label: 'Node 2' },
-					position: { x: 500, y: 700 },
-				},
-			]}
-		>
+		<ReactFlow snapGrid={[16, 16]} nodes={nodes} onNodesChange={onNodesChange}>
 			<Controls showInteractive={false} />
 			<Background variant={BackgroundVariant.Dots} gap={16} size={1} />
 		</ReactFlow>
